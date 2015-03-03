db6d67564a656e9eda17fbf3dd649ec9aa10daa3
7flying <diez.f.irene@gmail.com>
2015-03-03 16:07:03 +0100

    Start!

---
+++ 001-multiples-3-5/001-multiples.clj
@@ -0,0 +1,16 @@
+; Answer 1
+(defn sum-multiples-3-5
+  "Returns the sum of all the multiples of 3 or 5 below a given number."
+  [number]
+  (loop [current 1 sum 0]
+   ;(println (str "Current: " current " Sum: " sum))
+    (if (= current number)
+      sum
+      (if (or (= (mod current 3) 0) (= (mod current 5) 0))
+        (let [sum (+ sum current)] 
+          (recur (inc current) sum))
+        (recur (inc current) sum)))))
+
+(sum-multiples-3-5 1000)
+
+
