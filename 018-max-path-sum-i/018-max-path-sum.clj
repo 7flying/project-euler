d8a52ae43893bf3ebe2a6bebbc40b8762ec456e0
7flying <diez.f.irene@gmail.com>
2015-06-03 23:13:44 +0200

    Combo! problem 18 + 67
    - The algorithm of #18 can be reused for #67, just load the proper triangle

--- 018-max-path-sum-i/018-max-path-sum.clj
+++ 018-max-path-sum-i/018-max-path-sum.clj
@@ -6,14 +6,26 @@
     (doall (line-seq r))))
 
 (def triangle
-  (loop [triangle [] lines (get-lines "resources/018-triangle.txt")]
+  (loop [triangle [] lines (get-lines "../resources/018-triangle.txt")]
     (if (empty? lines)
       triangle
       (recur (conj triangle
-                   (map #(Integer/parseInt %) (split (first lines) #"\s+")))
+                   (into [] (doall (map #(Integer/parseInt %) (split (first lines) #"\s+")))) )
              (rest lines)))))
 
-triangle
-(reverse triangle)
-(partition 2 1 [1 2 3 4 5 6 7 8 9 10 11 12 13 14])
-(map #(apply max %) (partition 2 1 [1 2 3 4 5 6 7 8 9 10 11 12 13 14]))
+(defn add-max-nums-to-row [from to]
+  (loop [newrow []
+         old to
+         sum (first (vector (map #(reduce max %) (partition 2 1 from))))]
+    (if (empty? old)
+      newrow
+      (recur (conj newrow (+ (first old) (first sum))) (rest old) (rest sum)))))
+
+(defn max-path-sum []
+  (loop [tri (reverse triangle)]
+    (if (= (count tri) 1)
+      tri
+      (recur (conj (rest (rest tri)) (add-max-nums-to-row (first tri) (first (rest tri))))))))
+
+(max-path-sum)
+
---
+++ 067-max-path-sum-ii/067-max-path-sum.clj
@@ -0,0 +1,2 @@
+; Use 018-max-path-sum.clj loading the triangle for this problem.
+; yeah!
