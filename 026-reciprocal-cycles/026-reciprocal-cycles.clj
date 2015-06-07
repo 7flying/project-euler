7763c943554d460dbe989a2d994646a9f5e9d228
7flying <diez.f.irene@gmail.com>
2015-06-07 12:26:43 +0200

    Modify 026, add lets

--- 026-reciprocal-cycles/026-reciprocal-cycles.clj
+++ 026-reciprocal-cycles/026-reciprocal-cycles.clj
@@ -1,15 +1,17 @@
 (defn cycle-size [deno]
   (loop [nume 1 cy '()]
-    (if (some #{(mod nume deno)} cy)
-      (count cy)
-      (recur (* 10 (mod nume deno)) (conj cy (mod nume deno))))))
+    (let [res (mod nume deno)]
+      (if (some #{res} cy)
+        (count cy)
+        (recur (* 10 res) (conj cy res))))))
 
 (defn largest-reciprocal-cycle []
   (loop [n 999 largest 0 deno 999]
     (if (= 0 n)
       (list largest deno)
-      (if (> (cycle-size n) largest)
-        (recur (dec n) (cycle-size n) n)
-        (recur (dec n) largest deno)))))
+      (let [len (cycle-size n)]
+        (if (> len largest)
+          (recur (dec n) len n)
+          (recur (dec n) largest deno))))))
 
 (largest-reciprocal-cycle)
