ccbe091801ed82b9910d2339923554d190d943a0
7flying <diez.f.irene@gmail.com>
2015-06-12 23:31:13 +0200

    Modify 30, add 11

--- 030-digit-fifth-powers/030-digit-fifth-pow.clj
+++ 030-digit-fifth-powers/030-digit-fifth-pow.clj
@@ -1,5 +1,5 @@
-(def cache {"0" 0 "1" 1 "2" 32 "3" 243 "4" 1024 "5" 3125 "6" 7776 "7" 16807 "8" 32768
-            "9" 59049})
+(def cache {"0" 0 "1" 1 "2" 32 "3" 243 "4" 1024 "5" 3125 "6" 7776 "7" 16807
+            "8" 32768 "9" 59049})
 
 (defn number-fifth-power-sum? [num]
   (loop [num num sum 0 origi num]
@@ -15,7 +15,7 @@
         (recur (conj numbers current) (inc current))
         (recur numbers (inc current))))))
 
-(digit-fifth-pow)
+(reduce + (digit-fifth-pow))
 
 
 
